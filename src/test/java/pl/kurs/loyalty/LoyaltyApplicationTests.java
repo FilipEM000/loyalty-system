10cb44fbbc10f6d6ec8638e8ee281e0bd0cf4c1c
Filip <filip.ostrowicki@wp.pl>
2026-08-29 14:41:36 +0200

    initial commit

---
+++ src/test/java/pl/kurs/loyalty/LoyaltyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kurs.loyalty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
